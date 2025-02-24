e6757da14808d43488c8339e4b391502050e02c7
mailbak36@gmail.com <mailbak36@gmail.com>
2025-02-24 23:43:40 +0500

    unit tests with coverage is setup

--- branch/tests.py
+++ branch/tests.py
@@ -1,7 +1,7 @@
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
-from core.models import Branch, Company, User
+from core.models import Branch, Company, User, CompanyUserRelation
 from rest_framework_simplejwt.tokens import AccessToken
 
 class BranchTests(APITestCase):
@@ -9,40 +9,45 @@ class BranchTests(APITestCase):
     def setUp(self):
         self.company = Company.objects.create(name="Test Company")
         self.user = User.objects.create_user(username="testuser", password="testpassword")
+        CompanyUserRelation.objects.create(user=self.user, company=self.company)
         self.token = AccessToken.for_user(self.user)
-        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}')
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.token}', HTTP_X_TENANT_ID=self.company.id)
         self.branch = Branch.objects.create(name="Test Branch", company=self.company)
 
     def test_create_branch(self):
-        url = reverse('branch-list')
-        data = {'name': 'New Branch', 'company': self.company.id}
+        url = reverse('branch:branch-create')
+        data = {'name': 'New Branch', 'company': self.company.id, 'address': ''}
+        
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Branch.objects.count(), 2)
         self.assertEqual(Branch.objects.get(id=response.data['id']).name, 'New Branch')
 
     def test_list_branches(self):
-        url = reverse('branch-list')
+        url = reverse('branch:branch-list')
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data['results']), 1)
 
     def test_retrieve_branch(self):
-        url = reverse('branch-detail', args=[self.branch.id])
+        url = reverse('branch:branch-detail', args=[self.branch.id])
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['name'], self.branch.name)
 
     def test_update_branch(self):
-        url = reverse('branch-detail', args=[self.branch.id])
-        data = {'name': 'Updated Branch'}
+        url = reverse('branch:branch-edit', args=[self.branch.id])
+        data = {
+            'name': 'Updated Branch',
+            'company': self.company.id,
+        }
         response = self.client.put(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.branch.refresh_from_db()
         self.assertEqual(self.branch.name, 'Updated Branch')
 
     def test_delete_branch(self):
-        url = reverse('branch-detail', args=[self.branch.id])
+        url = reverse('branch:branch-detail', args=[self.branch.id])
         response = self.client.delete(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertEqual(Branch.objects.count(), 0)
+        self.assertEqual(Branch.objects.filter(is_deleted=False).count(), 0)
--- core/serializers.py
+++ core/serializers.py
@@ -3,38 +3,6 @@ from rest_framework import serializers
 from core.models import BaseUserCheck, Company
 from core.permissions import get_tenant_id
 
-class AuditSerializerMixin(serializers.ModelSerializer):
-    class Meta:
-        abstract = True
-
-    def create(self, validated_data):
-        request = self.context.get('request', None)
-        instance = self.Meta.model(**validated_data)
-        instance.save(request=request)
-
-        return instance
-
-    def update(self, instance, validated_data):
-        request = self.context.get('request', None)
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        instance.save(request=request)
-
-        return instance
-
-    def delete(self, instance):
-        request = self.context.get('request', None)
-
-        instance.delete(request=request)
-
-    def to_internal_value(self, data):
-        category_id = data.get('id', None)
-        validated_data = super().to_internal_value(data)
-        if category_id is not None:
-            validated_data['id'] = category_id
-
-        return validated_data
-    
 
 class BaseModelInputSerializer(serializers.ModelSerializer, BaseUserCheck):
     
